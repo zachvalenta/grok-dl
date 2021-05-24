6826f7efe1818da60b751d5a1d8aed0f3ef102a0
zachvalenta <zjv5002@gmail.com>
2021-05-24 14:05:40 -0400

    ch 3 - rf 1 in 1 out

--- chapter_3.py
+++ chapter_3.py
@@ -1,9 +1,6 @@
-# knicks, bulls, suns
-win_per = [0.5, 0.45, 0.7]
-
 ###
 # 1 input, 1 output [Trask 3.22-27]
 ###
 
-def chance_win_single(datapoint):
-    return datapoint * 1.0
+def chance_win_single(wp):
+    return wp * 1.0
--- test_chapter_3.py
+++ test_chapter_3.py
@@ -1,7 +1,21 @@
+from collections import namedtuple
+
 import pytest
 
 from chapter_3 import chance_win_single
 
-def test_chance_win_single():
-    assert chance_win_single(datapoint=0.5) == 0.5
-    assert chance_win_single(datapoint=0.7) == 0.7
+@pytest.fixture(scope="module")
+def data():
+    Team = namedtuple('team', 'win_per fans')
+    bulls = Team(win_per=0.45, fans=2.5)
+    knicks = Team(win_per=0.5, fans=8.5)
+    suns = Team(win_per=0.7, fans=0.75)
+    return {
+        "bulls": bulls,
+        "knicks": knicks,
+        "suns": suns,
+    }
+
+def test_chance_win_single(data):
+    assert chance_win_single(wp=data["knicks"].win_per) == 0.5
+    assert chance_win_single(wp=data["suns"].win_per) == 0.7
