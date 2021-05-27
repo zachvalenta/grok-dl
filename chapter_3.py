d04fe3d2d67cd3bd65692fac615fbe2a01da0228
zachvalenta <zjv5002@gmail.com>
2021-05-27 11:18:14 -0400

    fmt

--- chapter_3.py
+++ chapter_3.py
@@ -2,12 +2,15 @@
 # 1 input, 1 output [Trask 3.22-27]
 ###
 
+
 def chance_win_single(dp, weight):
     return dp * weight
+
 
 ###
 # n input, 1 output [Trask 3.28-35]
 ###
+
 
 def chance_win_weighted(dps, weights):
     prediction = 0
--- test_chapter_3.py
+++ test_chapter_3.py
@@ -4,9 +4,10 @@ import pytest
 
 from chapter_3 import chance_win_single, chance_win_weighted
 
+
 @pytest.fixture(scope="module")
 def data():
-    Team = namedtuple('team', 'win_per fans')
+    Team = namedtuple("team", "win_per fans")
     bulls = Team(win_per=0.45, fans=2.5)
     knicks = Team(win_per=0.5, fans=8.5)
     suns = Team(win_per=0.7, fans=0.75)
@@ -16,16 +17,22 @@ def data():
         "suns": suns,
     }
 
+
 def test_chance_win_single(data):
     assert chance_win_single(dp=data["knicks"].win_per, weight=1.0) == 0.5
     assert chance_win_single(dp=data["suns"].win_per, weight=1.0) == 0.7
 
+
 def test_chance_win_weighted_sum(data):
-    assert chance_win_weighted(
-        dps=[data["knicks"].win_per, data["knicks"].fans],
-        weights=[0.75, 0.05]
-    ) == 0.8
-    assert chance_win_weighted(
-        dps=[data["suns"].win_per, data["suns"].fans],
-        weights=[0.75, 0.05]
-    ) == 0.6
+    assert (
+        chance_win_weighted(
+            dps=[data["knicks"].win_per, data["knicks"].fans], weights=[0.75, 0.05]
+        )
+        == 0.8
+    )
+    assert (
+        chance_win_weighted(
+            dps=[data["suns"].win_per, data["suns"].fans], weights=[0.75, 0.05]
+        )
+        == 0.6
+    )
